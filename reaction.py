2db31afb6db9afb5ab3b88d7c0a790e75c03f1b6
liver-51 <20119476@setu.ie>
2026-04-09 15:19:12 +0800

    Step3: Add button detection

--- reaction.py
+++ reaction.py
@@ -1,8 +1,20 @@
-from gpiozero import LED
+from gpiozero import LED, Button
 from time import sleep
+from random import uniform
 
 led = LED(4)
+left_btn = Button(14)
+right_btn = Button(15)
+
 led.on()
-sleep(5)
+sleep(uniform(5, 10))
 led.off()
 
+def pressed(btn):
+    print(f"Button {btn.pin.number} pressed!")
+
+left_btn.when_pressed = pressed
+right_btn.when_pressed = pressed
+
+while True:
+    sleep(0.1)
